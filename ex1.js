02c26c9387bdbdefe5354cb2ec44f2bf1f190f37
Nguyen Tien Minh <nguyentienminhntm07102004@gmail.com>
2024-04-22 20:05:08 +0700

    Nop bai lan 2

--- ex1.js
+++ ex1.js
@@ -1,5 +1,8 @@
 let findAvg = (arr) => {
-  return arr.reduce((total, value) => {
-    return total + (value / arr.length);
-  }, 0);
+  let avg = 0;
+  arr.forEach((value) => {
+    avg += (value / arr.length);
+  });
+  return avg;
 };
+console.log(findAvg([1, 2, 5, 7, 9]));
